4709545ebfeaddb96397a0aad5ce7a7659ed2bf9
Kevin Dalton <kmdalton@pop-os.localdomain>
2023-09-16 13:04:57 -0400

    more asus please

--- scramble/symmetry/reciprocal_asu.py
+++ scramble/symmetry/reciprocal_asu.py
@@ -2,15 +2,69 @@ import gemmi
 import reciprocalspaceship as rs
 import torch
 import numpy as np
+from scramble.symmetry import Op
+
+class ReciprocalASUCollection(torch.nn.Module):
+    def __init__(self, reciprocal_asus, dtype=None, device=None):
+        super().__init__()
+        self.asu_size = 0
+        self.hmax = 0
+        self.recprocal_asus = reciprocal_asus
+        for rasu in reciprocal_asus:
+            self.asu_size += rasu.asu_size
+            self.hmax = np.maximum(self.hmax, rasu.hmax).tolist()
+
+        miller_id = []
+        asu_id = []
+        asu_start = 0
+        for rasu in reciprocal_asus:
+            asu_miller_id = torch.arange(len(rasu.Hasu), device=device, dtype=torch.int)
+            asu_miller_id = asu_miller_id + asu_start
+            miller_id.append(
+                rasu.to_voxel_grid(asu_miller_id, self.hmax)
+            )
+            asu_start = asu_start + rasu.asu_size
+
+        miller_id = torch.stack(miller_id)
+        self.miller_id = torch.nn.Parameter(miller_id, requires_grad=False)
+
+    @property
+    def centric(self):
+        return torch.concat([rasu.centric for rasu in self.recprocal_asus])
+
+    @property
+    def multiplicity(self):
+        return torch.concat([rasu.multiplicity for rasu in self.recprocal_asus])
+
+    def __iter__(self):
+        return self.reciprocal_asus.__iter__()
+
+    def __next__(self):
+        return self.reciprocal_asus.__next__()
+
+    def __len__(self):
+        return len(self.reciprocal_asus)
+
+    def forward(self, asu_id, hkl):
+        h,k,l = hkl[...,0], hkl[...,1], hkl[...,2]
+        return self.miller_id[asu_id, h, k, l]
 
 class ReciprocalASU(torch.nn.Module):
+    @rs.decorators.cellify
+    @rs.decorators.spacegroupify
     def __init__(self, cell, spacegroup, dmin, anomalous=False, dtype=None, device=None):
         super().__init__()
-        self.Hasu = rs.utils.generate_reciprocal_asu(cell, spacegroup, dmin, anomalous)
+        self.anomalous = anomalous
+        self.Hasu = torch.nn.Parameter(
+            torch.tensor(
+                rs.utils.generate_reciprocal_asu(cell, spacegroup, dmin, anomalous),
+                device=device,
+                dtype=torch.int,
+            ),
+            requires_grad=False,
+        )
         self.asu_size = len(self.Hasu)
         self.hmax = cell.get_hkl_limits(dmin)
-        h,k,l = self.hmax
-        miller_id = -np.ones((2*h + 1, 2*k + 1, 2*l + 1), dtype='int')
         self.spacegroup = spacegroup
         self.centric = torch.nn.Parameter(
             torch.tensor(
@@ -28,21 +82,33 @@ class ReciprocalASU(torch.nn.Module):
             ),
             requires_grad=False,
         )
-
-        for op in spacegroup.operations():
-            Hop = rs.utils.apply_to_hkl(self.Hasu, op)
-            h,k,l = Hop.T
-            miller_id[h, k, l] = np.arange(len(self.Hasu))
-
-            #Now do Friedel
-            if not anomalous:
-                h,k,l = -Hop.T
-                miller_id[h, k, l] = np.arange(len(self.Hasu))
-
+        self.ops = [Op(op) for op in self.spacegroup.operations()]
+        miller_id = self.to_voxel_grid(torch.arange(len(self.Hasu), device=device, dtype=torch.int))
         self.miller_id = torch.nn.Parameter(
-            torch.tensor(miller_id, dtype=torch.int, device=device),
+            miller_id,
             requires_grad=False,
         )
+
+    def to_voxel_grid(self, asu_values, hkl_limits=None, fill_value=-1):
+        dtype = asu_values.dtype
+        device = asu_values.device
+        if hkl_limits is None:
+            hkl_limits = self.hmax
+        h,k,l = hkl_limits
+        voxel_grid = fill_value * torch.ones(
+            (2*h + 1, 2*k + 1, 2*l + 1), 
+            dtype=dtype, device=device,
+        )
+        for op in self.ops:
+            Hop = op(self.Hasu)
+            h,k,l = Hop.T
+            voxel_grid[h, k, l] = asu_values
+
+            #Now do Friedel
+            if not self.anomalous:
+                h,k,l = -Hop.T
+                voxel_grid[h, k, l] = asu_values
+        return voxel_grid
 
     def forward(self, hkl):
         h,k,l = hkl[...,0],hkl[...,1],hkl[...,2]
@@ -50,3 +116,15 @@ class ReciprocalASU(torch.nn.Module):
 
 
 
+if __name__=="__main__":
+    cell = [34., 45., 98., 90., 90., 90.]
+    sg = 19
+    dmin = 1.8
+    anomalous = True
+    rasu = ReciprocalASU(cell, sg, dmin, anomalous=anomalous)
+
+    r1 = ReciprocalASU(cell, sg, 5., anomalous=anomalous)
+    r2 = ReciprocalASU(cell, sg, 2., anomalous=anomalous)
+    rac = ReciprocalASUCollection((r1, r2))
+    from IPython import embed
+    embed(colors='linux')
