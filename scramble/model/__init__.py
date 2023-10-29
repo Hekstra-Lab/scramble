e93f6892827a503023320b449481c79a15c86be8
Kevin Dalton <kmdalton@pop-os.localdomain>
2023-10-29 19:52:33 -0400

    refactor imports

--- scramble/model/__init__.py
+++ scramble/model/__init__.py
@@ -1,3 +1,5 @@
-from .model import MergingModel,ScalingModel,NormalLikelihood
+from .merging import MergingModel
+from .scaling import ScalingModel
+from .likelihood import NormalLikelihood
 from .surrogate_posterior import SurrogatePosterior
 from .mlp import Linear,MLP,FeedForward
